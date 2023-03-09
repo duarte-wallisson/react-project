b1f4632989765beaf45b273ae97f2179ac7c2fb1
duarte-wallisson <wallisson.elduarte@gmail.com>
2023-03-08 22:07:33 -0300

    Teste da sintaxe de componentes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-
+import HelloWorld from "./components/HelloWorld";
+import Phrase from "./components/Phrase";
 function App() {
     return (
         <div className="App">
             <h1> Hello World!</h1>
             <p> My first react project</p>
+            <HelloWorld/>
+            <Phrase/>
         </div>
     );
 }
---
+++ src/components/Phrase.js
@@ -0,0 +1,9 @@
+function Phrase() {
+    return (
+        <div>
+            <p>This is a phrase of component</p>
+        </div>
+    )
+}
+
+export default Phrase
